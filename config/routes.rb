1d7d7c1ffb827e455bd9472adb99de2638ef1865
tako <rina.m.takase@gmail.com>
2017-05-06 18:48:02 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users,  controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  get 'dashboard/index'
+  root to: 'dashboard#index'
 
-  get '/auth/:provider/callback', to: 'sessions#create'
+  get 'landing/index', as: 'new_user_session'
+
+  get 'dashboard/index'
 
   devise_scope :user do
     get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session

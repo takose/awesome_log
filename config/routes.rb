f19f7eb2632da6b41302a3b165af797c4317eade
tako <rina.m.takase@gmail.com>
2017-05-06 18:28:53 +0900

    Modify authentication to use devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'landing/index'
+  devise_for :users,  controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   get 'dashboard/index'
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
+  devise_scope :user do
+    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20170503103121) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "provider"
     t.string   "uid"
     t.string   "oauth_token"
     t.string   "oauth_token_secret"

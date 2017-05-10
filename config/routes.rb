f48e89d30040e996de50f6655941cacd9e0b51d0
tako <rina.m.takase@gmail.com>
2017-05-10 16:53:00 +0900

    Settings page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'dashboard#index'
 
   get '/welcome', to: 'landing#index', as: 'sign_in'
+  get '/settings', to: 'users#edit', as: 'settings'
 
   get '/auth/:provider/callback', to: 'sessions#create'
 end

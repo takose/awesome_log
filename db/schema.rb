71126961960e7b37b150348926baff92e0f0890b
tako <rina.m.takase@gmail.com>
2017-05-23 20:57:39 +0900

    create service model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510173631) do
+ActiveRecord::Schema.define(version: 20170510174452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20170510173631) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "service_id"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.integer  "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

1738a41815ebb9166d5fa930aa5075479ad83da6
tako <rina.m.takase@gmail.com>
2017-05-24 15:49:38 +0900

    Implement register of service

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: :edit
 
   def edit
+    @registered_services = current_user.services.all
+    @unregistered_services = Service.all - @registered_services
   end
 
   private
